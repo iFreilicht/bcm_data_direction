d07d82d26b5cfa451af337735de6116529d4a2dd
iFreilicht <felix.uhl@outlook.com>
2017-08-18 23:36:08 +0200

    Generalise loading and storing
    This allows to store to/load from any forward-iterable object like a serial interface or a virtual file.

--- storage.h
+++ storage.h
@@ -95,94 +95,102 @@ namespace storage{
             schedule_indices.size() * sizeof(schedule_indices[0]);
 
     //Additional info stored in EEPROM
-    struct eeprom_header{
+    struct header_t{
         //This value is important to differentiate between byte data of cues and schedules
         size_t number_of_cues;
         size_t number_of_schedule_elements;
     };
 
-    //Stores all cues and schedules to eeprom
-    void store_all_in_eeprom(){
-        size_t total_size = size_in_bytes() + sizeof(eeprom_header);
-        if(total_size > EEPROM.length()){
+
+    //Write all cues and schedules as binary data starting at an iterator
+    //If maximum_size is specified, writing will only take place if the data fits
+    template<typename iteratorT>
+    void store_all(iteratorT begin, size_t maximum_size = 0){
+        size_t total_size = size_in_bytes() + sizeof(header_t);
+        if(maximum_size && total_size > maximum_size){
             printf("ERROR: Can't write %u bytes to EEPROM, it's only %u bytes long.", total_size, EEPROM.length());
             return;
         }
 
-        eeprom_header header = { loaded_cues.size(), loaded_schedules.size() };
+        header_t header = { loaded_cues.size(), loaded_schedules.size() };
 
-        //Iterator for the whole EEPROM
-        auto eeprom_iter = EEPROM.begin();
+        //Iterator for the whole range
+        iteratorT store_iter = begin;
 
-        //Write header to EEPROM
+        //Write header to range
         const uint8_t* header_byte_iter = static_cast<const uint8_t*>(static_cast<void*>(&header));
-        auto eeprom_header_end = EEPROM.begin() + sizeof(header);
 
-        for(; eeprom_iter < eeprom_header_end; ++eeprom_iter, ++header_byte_iter){
-            (*eeprom_iter).update(*header_byte_iter);
+        for(int store_byte = 0; store_byte < sizeof(header); ++store_byte, ++store_iter, ++header_byte_iter){
+            *store_iter = *header_byte_iter;
         }
 
-        //Write cues to EEPROM
+        //Write cues to range
         const uint8_t* cues_byte_iter = static_cast<const uint8_t*>(static_cast<void*>(&*loaded_cues.begin()));
-        auto eeprom_cues_end = eeprom_header_end + sizeof(Cue) * loaded_cues.size();
 
-        for(; eeprom_iter < eeprom_cues_end; ++eeprom_iter, ++cues_byte_iter){
-            (*eeprom_iter).update(*cues_byte_iter);
+        for(int store_byte = 0; store_byte < sizeof(Cue) * loaded_cues.size(); ++store_byte, ++store_iter, ++cues_byte_iter){
+            *store_iter = *cues_byte_iter;
         }
 
-        //Write schedules to EEPROM
+        //Write schedules to range
         const uint8_t* schedules_byte_iter = static_cast<const uint8_t*>(static_cast<void*>(&*loaded_schedules.begin()));
-        auto eeprom_schedules_end = eeprom_cues_end + sizeof(delay_t) * loaded_schedules.size();
 
-        for(; eeprom_iter < eeprom_schedules_end; ++eeprom_iter, ++schedules_byte_iter){
-            (*eeprom_iter).update(*schedules_byte_iter);
+        for(int store_byte = 0; store_byte < sizeof(delay_t) * loaded_schedules.size(); ++store_byte, ++store_iter, ++schedules_byte_iter){
+            *store_iter  = *schedules_byte_iter;
         }
     }
 
-    //Loads all cues and scheduels stored in EEPROM.
-    //WARNING! This will automatically clear cues and schedules!
-    void load_all_from_eeprom(){
+    //Stores all cues and schedules to eeprom
+    void store_all_in_eeprom(){
+        store_all(EEPROM.begin(), EEPROM.length());
+    }
+
+    //Load all cues and schedules from binary data between two input-iterators
+    template<typename iteratorT>
+    void load_all(iteratorT begin, iteratorT end){
         clear_cues();
         clear_schedules();
 
-        eeprom_header header;
+        //Iterator for the whole range
+        iteratorT store_iter = begin;
 
-        //Iterator for the whole EEPROM
-        auto eeprom_iter = EEPROM.begin();
-
-        //Read header from EEPROM
+        //Read header from range
+        header_t header;
         uint8_t* header_byte_iter = static_cast<uint8_t*>(static_cast<void*>(&header));
-        auto eeprom_header_end = EEPROM.begin() + sizeof(header);
 
-        for(; eeprom_iter < eeprom_header_end; ++eeprom_iter, ++header_byte_iter){
-            *header_byte_iter = **eeprom_iter;
+        for(int store_byte = 0; store_byte < sizeof(header); ++store_byte, ++store_iter, ++header_byte_iter){
+            if(store_iter >= end) return;
+            *header_byte_iter = *store_iter;
         }
 
-        //Load cues from EEPROM
-        auto eeprom_cues_end = eeprom_header_end + sizeof(Cue) * header.number_of_cues;
-
-        while(eeprom_iter < eeprom_cues_end){
+        //Load cues from range
+        for(int store_byte = 0; store_byte < sizeof(Cue) * header.number_of_cues;){
             //Build full cue to allow pushing it properly
             Cue cue;
-            for(int byte_position = 0; byte_position < sizeof(Cue); ++byte_position, ++eeprom_iter){
-                static_cast<uint8_t*>(static_cast<void*>(&cue))[byte_position] = **eeprom_iter;
+            for(int cue_byte = 0; cue_byte < sizeof(Cue); ++store_byte, ++cue_byte, ++store_iter){
+                if(store_iter >= end) return;
+                static_cast<uint8_t*>(static_cast<void*>(&cue))[cue_byte] = *store_iter;
             }
 
             push_cue(cue);
         }
 
-        //Write schedules to EEPROM
-        auto eeprom_schedules_end = eeprom_cues_end + sizeof(delay_t) * header.number_of_schedule_elements;
-
-        while(eeprom_iter < eeprom_schedules_end){
+        //Write schedules to range
+        for(int store_byte = 0; store_byte < sizeof(delay_t) * header.number_of_schedule_elements;){
             //Build full schedule element to allow pushing it properly
             delay_t schedule_element;
-            for(int byte_position = 0; byte_position < sizeof(delay_t); ++byte_position, ++eeprom_iter){
-                static_cast<uint8_t*>(static_cast<void*>(&schedule_element))[byte_position] = **eeprom_iter;
+            for(int schedule_byte = 0; schedule_byte < sizeof(delay_t); ++store_byte, ++schedule_byte, ++store_iter){
+                if(store_iter >= end) return;
+                static_cast<uint8_t*>(static_cast<void*>(&schedule_element))[schedule_byte] = *store_iter;
             }
 
             push_schedule(schedule_element);
         }
+    }
+
+        //Loads all cues and scheduels stored in EEPROM.
+    //WARNING! This will automatically clear cues and schedules!
+    void load_all_from_eeprom(){
+        load_all(EEPROM.begin(), EEPROM.end());
     }
 }
 }
